c27cb215fa4ff5281870e6a2b6250be11b4efef1
dell <dell@gmail.com>
2024-08-10 23:57:23 +0100

    added python-selenium/

--- python-selenium/app.py
+++ python-selenium/app.py
@@ -1,13 +1,15 @@
+import time
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element(By.NAME, "q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+browser = webdriver.Chrome()
+browser.get("https://nextdoor.co.uk/news_feed")
+
+#search = browser.find_element(By.NAME, "q")
+#search.send_keys("selenium")
+#search.send_keys(Keys.RETURN) # hit return after you enter search text
+time.sleep(25) # sleep for 5 seconds so you can see the results
+browser.quit()
