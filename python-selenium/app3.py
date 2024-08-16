a5a86963b6bbc6467cd8dcb6b4002a1adbb28126
dell <dell@gmail.com>
2024-08-16 22:34:59 +0100

    python-selenium navigations

--- python-selenium/app3.py
+++ python-selenium/app3.py
@@ -1,23 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 import time
 
-username = "username"
-password = "password"
 driver = webdriver.Chrome()
 driver.get("https://nextdoor.co.uk/news_feed")
+options = webdriver.ChromeOptions() #newly added 
+options.add_argument("--headless") #newly added 
 
 
 time.sleep(3)
 search_box = driver.find_element(By.ID, 'didomi-notice-agree-button')
 search_box.send_keys(Keys.RETURN)
-time.sleep(3)
-driver.find_element("id", "id_email").send_keys(username)
-driver.find_element("id", "id_password").send_keys(password)
+time.sleep(5)
+search_box = driver.find_element(By.ID, 'google-button')
+search_box.send_keys(Keys.RETURN)
+time.sleep(35)
 
-time.sleep(13)
 # You can now interact with the search results using Selenium
 # For example, you could click on a specific link or extract information
 
